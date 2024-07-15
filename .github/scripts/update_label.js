592569b95b8d8cc8a7803ea2169dec4ec85c825c
Guillaume Monrolin <gmonrolin@r-yogi.com>
2024-07-15 15:25:34 +0200

    update workflows

--- .github/scripts/update_label.js
+++ .github/scripts/update_label.js
@@ -1,8 +1,7 @@
 import { Octokit } from "octokit"
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const REPO_OWNER = process.env.REPO_OWNER;
-const REPO_NAME = process.env.REPO_NAME;
+const REPO = process.env.REPO;
 const ISSUE_ID = process.env.ISSUE_ID;
 const LABEL_REVIEW_REQUESTED = 'review requested';
 const LABEL_APPROVED = 'approved';
@@ -18,9 +17,8 @@ const headers = {
 };
 
 async function getReviewsForPR() {
-    const response = await octokit.request('GET /repos/{owner}/{repo}/pulls/{issue_number}/reviews', {
-      owner: REPO_OWNER,
-      repo: REPO_NAME,
+    const response = await octokit.request('GET /repos/{repo}/pulls/{issue_number}/reviews', {
+      repo: REPO,
       issue_number: ISSUE_ID,
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
@@ -30,9 +28,8 @@ async function getReviewsForPR() {
 }
 
 async function addLabelToPR(label) {
-    const response = await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-      owner: REPO_OWNER,
-      repo: REPO_NAME,
+    const response = await octokit.request('POST /repos/{repo}/issues/{issue_number}/labels', {
+      repo: REPO,
       issue_number: ISSUE_ID,
       labels: [label],
       headers: {
@@ -43,9 +40,8 @@ async function addLabelToPR(label) {
 }
 
 async function getLabelFromPR() {
-  const response = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: REPO_OWNER,
-    repo: REPO_NAME,
+  const response = await octokit.request('GET /repos/{repo}/issues/{issue_number}/labels', {
+    repo: REPO,
     issue_number: ISSUE_ID,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
@@ -55,9 +51,8 @@ async function getLabelFromPR() {
 }
 
 async function removeLabelFromPR(label) {
-    const response = await octokit.request('DELETE /repos/{owner}/{repo}/issues/{issue_number}/labels/{label}', {
-      owner: REPO_OWNER,
-      repo: REPO_NAME,
+    const response = await octokit.request('DELETE /repos/{repo}/issues/{issue_number}/labels/{label}', {
+      repo: REPO,
       issue_number: ISSUE_ID,
       label: label,
       headers: {
